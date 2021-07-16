f7113618bffecdeeb9093875d85df0bef7877fb8
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2021-07-15 21:12:53 -0400

    Update balanced_brackets.cs
    Found an alternative method by using  a stack in c# to store all of the opening brackets in the string. If the closing bracket did not match the top of the stack, we can assume that the brackets were not balanced.

--- Strings/balanced_brackets.cs
+++ Strings/balanced_brackets.cs
@@ -22,4 +22,49 @@ class MainClass {
             return false;
         }
     }
+
+    public static bool isBalanced_UsingStacks(string str) {
+        
+        var s = new Stack();
+        char x;
+        
+        for(int i = 0; i < str.Length; i++){
+            // Check for all opening brackets, ad push them onto the stack.
+            if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+                s.Push(str[i]);
+                continue;
+            }
+            
+            // If current char is not opening, then it must be closing. 
+            if(s.Count == 0) return false;
+            
+            switch(str[i]){
+                case ')':
+                    x = (char)s.Peek();
+                    s.Pop();
+                    if(x == '{' || x == '['){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    x = (char)s.Peek();
+                    s.Pop();
+                    if(x == '(' || x == '['){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    x = (char)s.Peek();
+                    s.Pop();
+                    if(x == '(' || x == '{'){
+                        return false;
+                    }
+                    break;
+                    
+            }
+            
+        }
+        return s.Count == 0;
+        
+    }
 }
