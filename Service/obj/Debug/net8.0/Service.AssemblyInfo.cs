00e62a9e7fdfc68db377e1eed73290c9df0f8112
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-05-28 22:08:00 -0400

    Cleanup

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6793a32aefceedcf49054eb26d341e6a8a970a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8794613a96e6ae017e86b36bb7023772533d20ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Trees/node.cs
+++
@@ -1,9 +0,0 @@
-class node {
-    int key;
-    Node left, right;
-    public Node(int item){
-        key = item;
-        left = right = null;
-    }
-}
-
