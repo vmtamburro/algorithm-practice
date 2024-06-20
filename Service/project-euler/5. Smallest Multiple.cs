d45a24f1cf39c25f56c5d41dda5e7d211a12a616
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-06-19 20:52:06 -0400

    Provide comments on smallest multiple algorithm

--- Service/project-euler/5. Smallest Multiple.cs
+++ Service/project-euler/5. Smallest Multiple.cs
@@ -7,11 +7,14 @@ public class Euler5{
     
         1. Start at lower limit Least Common Multiple
         2. Check divisibility
-        3. Increment by the highest number in range (20)    
+        3. Increment by the highest number in range (20)  
+
+
+        Solution is O(n log n) because w loop through iterating numbers, and the GCD calculation is logarithmic  
     */
 
     public int SmallestMultiple(){
-        int lcm = 0;
+        int lcm = 1; // Initialize to 1 because LCM with 0 is always technically 0.
         for(var i = 1; i <= 20; i++)
         {
             lcm = LCM(lcm, i);
@@ -19,10 +22,18 @@ public class Euler5{
         return lcm;
     }
 
+
+
+    /*
+        Calculates least common multiple of two different integers
+    */
     public int LCM(int a, int b){
         return (a  * b) / GCD(a, b);
     }
 
+    /*
+        Euclidean algorithm. Calculates the Greatest Common Divisor.
+    */
     public int GCD(int a, int b){
         while(b != 0){
             var temp = b;
