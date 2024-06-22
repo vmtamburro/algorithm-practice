025ae64940e66c1808049739a0f5148c398ad679
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-06-22 16:44:00 -0400

    Factorial Digit Sum Solution

---
+++ Service/project-euler/20. Factorial Digit Sum.cs
@@ -0,0 +1,37 @@
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+public class Euler20{
+    /*
+        n! means n × (n − 1) × ... × 3 × 2 × 1
+
+        For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+        and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+        Find the sum of the digits in the number 100!
+    */
+
+    public int FactorialDigitSum(){
+        BigInteger factorial = CalculateFactorial(n);
+        
+        int sumOfDigits = SumDigits(factorial);
+        return sumOfDigits;
+    }
+
+    public BigInteger CalculateFactorial(int n){
+        BigInteger factorial = 1;
+        for(int i = 2; i <= n; i++){
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    private int SumDigits(BigInteger number){
+        string numberStr = number.ToString();
+        int sum = 0;
+        foreach(char c in numberStr){
+            sum += (int)Char.GetNumericValue(c);
+        }
+        return sum;
+    }
+}
