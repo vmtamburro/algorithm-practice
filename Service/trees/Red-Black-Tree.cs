24e38c800781c25be4fde799beefa169e86271c9
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-02 21:59:08 -0400

    Add insertion method

--- Service/trees/Red-Black-Tree.cs
+++ Service/trees/Red-Black-Tree.cs
@@ -26,5 +26,79 @@ public class RedBlackTree{
         root = null;
     }
 
-    
+    // Insertion Method
+
+    public void Insert(int data){
+        root = Insert(root, data);
+        root.Color = NodeColor.Black; // Ensure the root is always black
+    }
+
+    private Node Insert(Node node, int data){
+        if(node == null){
+            return new Node(data); //generating new tree
+        }
+
+        // traverse down to point of insertion
+        if(data < node.Data){
+            node.Left = Insert(node.Left, data);
+        }
+        else if(data > node.Data){
+            node.Right = Insert(node.Right, data);
+        }
+        else{
+            // duplicate value, don't allow insertion (depends on requirements)
+            return node;
+        }
+
+
+        // Perform rotations and recoloring to maintain red-black tree properties
+        if(IsRed(node.Right) && !IsRed(node.Left)){
+            node = RotateLeft(node);
+        }
+        if(IsRed(node.Left) && IsRed(node.Left.Left)){
+            node = RotateRight(node);
+        }
+        if(IsRed(node.Left) && IsRed(node.Right)){
+            FlipColors(node);
+        }
+
+        return node;
+    }
+
+    // Helper methods for rotations and color flipping
+    private bool IsRed(Node node)
+    {
+        if (node == null)
+        {
+            return false; // Null nodes are considered black
+        }
+        return node.Color == NodeColor.Red;
+    }
+
+    private Node RotateLeft(Node node)
+    {
+        Node temp = node.Right;
+        node.Right = temp.Left;
+        temp.Left = node;
+        temp.Color = node.Color;
+        node.Color = NodeColor.Red;
+        return temp;
+    }
+
+    private Node RotateRight(Node node)
+    {
+        Node temp = node.Left;
+        node.Left = temp.Right;
+        temp.Right = node;
+        temp.Color = node.Color;
+        node.Color = NodeColor.Red;
+        return temp;
+    }
+
+    private void FlipColors(Node node)
+    {
+        node.Color = NodeColor.Red;
+        node.Left.Color = NodeColor.Black;
+        node.Right.Color = NodeColor.Black;
+    }
 }
