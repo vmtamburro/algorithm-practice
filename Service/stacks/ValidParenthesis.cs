a08e7e24a5831c2535370ddb69a947ae07daf861
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-17 23:33:50 -0400

    add isValid logic

--- Service/stacks/ValidParenthesis.cs
+++ Service/stacks/ValidParenthesis.cs
@@ -11,6 +11,27 @@ public class ValidParenthesis{
         }
     }
     public static bool isValid(string s){
-        
+        Stack<char> stack = new Stack<char>();
+
+        Dictionary<char, char> mappings = new Dictionary<char, char>(){
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        foreach(char c in s){
+            if(mappings.ContainsKey(c)){
+                char topElement = stack.Count == 0 ? '#' : stack.Pop();
+                if(topElement != mappings[c]){
+                    return false;
+                }
+            }
+            else{
+                // if it's an opening bracket, push it onto the stack
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count == 0;
     }
 }
