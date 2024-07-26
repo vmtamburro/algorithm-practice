c82022503410892d0083e41f1f4d6386e78bf8c7
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-25 22:00:25 -0400

    hash table practice with open addressing

--- Service/random-practice/RandoPractice_HashTables.cs
+++ Service/random-practice/RandoPractice_HashTables.cs
@@ -13,21 +13,25 @@ public class RandoPractice_HashTables
         {
             table = new List<KeyValuePair<TKey, TValue>>[size];
             this.size = size;
-            for(int i = 0; i < size; i++)
+            for (int i = 0; i < size; i++)
             {
                 table[i] = new List<KeyValuePair<TKey, TValue>>();
             }
         }
 
-        public int GetIndex(TKey key){
+        public int GetIndex(TKey key)
+        {
             return Math.Abs(key.GetHashCode()) % size;
         }
 
-        public void Insert(TKey key, TValue value){
+        public void Insert(TKey key, TValue value)
+        {
             int index = GetIndex(key);
             var bucket = table[index];
-            foreach(var pair in bucket){
-                if(pair.Key.Equals(key)){
+            foreach (var pair in bucket)
+            {
+                if (pair.Key.Equals(key))
+                {
                     bucket.Remove(pair);
                     break;
                 }
@@ -37,11 +41,14 @@ public class RandoPractice_HashTables
         }
 
 
-        public bool Delete(TKey key){
+        public bool Delete(TKey key)
+        {
             int index = GetIndex(key);
             var bucket = table[index];
-            foreach(var pair in bucket){
-                if(pair.Key.Equals(key)){
+            foreach (var pair in bucket)
+            {
+                if (pair.Key.Equals(key))
+                {
                     bucket.Remove(pair);
                     return true;
                 }
@@ -50,11 +57,14 @@ public class RandoPractice_HashTables
             return false;
         }
 
-        public TValue Search(TKey key){
+        public TValue Search(TKey key)
+        {
             int index = GetIndex(key);
             var bucket = table[index];
-            foreach(var pair in bucket){
-                if(pair.Key.Equals(key)){
+            foreach (var pair in bucket)
+            {
+                if (pair.Key.Equals(key))
+                {
                     return pair.Value;
                 }
             }
@@ -63,5 +73,89 @@ public class RandoPractice_HashTables
         }
 
 
+    }
+
+    public class HashTable_OpenAddressing<TKey, TValue>
+    {
+        private (TKey, TValue)[] table;
+        private int size;
+        private int count;
+        public HashTable_OpenAddressing(int size)
+        {
+            table = new (TKey, TValue)[size];
+            this.size = size;
+            count = 0;
+        }
+        private int GetIndex(TKey key, int attempt)
+        {
+            int hash = Math.Abs(key.GetHashCode());
+            return (hash + attempt) % size;
+        }
+
+        public void Insert(TKey key, TValue value)
+        {
+            if (count >= size * 0.7)
+            {
+                throw new InvalidOperationException("Table is full");
+            }
+
+            int attempt = 0;
+            int index = GetIndex(key, attempt);
+
+            while (true)
+            {
+                index = GetIndex(key, attempt);
+                if (table[index] == (null, null) || table[index].Item1.Equals(key))
+                {
+                    table[index] = (key, value);
+                    count++;
+                    return;
+                }
+                attempt++;
+            }
+        }
+
+        public bool Delete(TKey key)
+        {
+            int attempt = 0;
+            int index;
+
+            while (true)
+            {
+                index = GetIndex(key, attempt);
+                if (table[index] == (null, null))
+                {
+                    return false;
+                }
+                if (table[index].Item1.Equals(key))
+                {
+                    table[index] = (default(TKey), default(TValue));
+                    count--;
+                    return true;
+                }
+                attempt++;
+            }
+        }
+
+
+        public TValue Lookup(TKey key)
+        {
+            int attempt = 0;
+            int index;
+            while (true)
+            {
+                index = GetIndex(key, attempt);
+                if (table[index] == (null, null))
+                {
+                    throw new KeyNotFoundException("Key not found");
+                }
+                if (table[index].Item1.Equals(key))
+                {
+                    return table[index].Item2;
+                }
+                attempt++;
+            }
+
+        }
     }
 }
