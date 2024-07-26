cfbda3d74938e783c545b20b0aef629718506bd0
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-25 22:38:53 -0400

    Remove custom priority queue implementation

--- Service/random-practice/RandoPractice_Graphs_Dijkstras.cs
+++ Service/random-practice/RandoPractice_Graphs_Dijkstras.cs
@@ -54,36 +54,5 @@ public class RandoPractice_Graphs_Dijkstras
         }
     }
 
-    // PriorityQueue implementation for .NET 6 or later
-    public class PriorityQueue<TKey, TValue> where TValue : IComparable
-    {
-        private SortedDictionary<TValue, Queue<TKey>> _dict = new SortedDictionary<TValue, Queue<TKey>>();
-
-        public int Count { get; private set; }
-
-        public void Enqueue(TKey key, TValue value)
-        {
-            if (!_dict.ContainsKey(value))
-                _dict[value] = new Queue<TKey>();
-
-            _dict[value].Enqueue(key);
-            Count++;
-        }
-
-        public TKey Dequeue()
-        {
-            if (Count == 0)
-                throw new InvalidOperationException("Queue is empty");
-
-            var firstPair = _dict.First();
-            var key = firstPair.Value.Dequeue();
-
-            if (firstPair.Value.Count == 0)
-                _dict.Remove(firstPair.Key);
-
-            Count--;
-            return key;
-        }
-    }
 
 }
