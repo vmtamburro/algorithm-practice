16ddc6738a6169f17e390884a6f801babd9d88a9
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-25 21:02:44 -0400

    insertion

--- Service/random-practice/arrays/RandoPractice_Arrays.cs
+++ Service/random-practice/arrays/RandoPractice_Arrays.cs
@@ -4,4 +4,24 @@ public class RandomPractice_Arrays{
         int element = array[2];
         Console.WriteLine(element);
     }
+
+    public void Insertion(){
+        int[] array = {1, 2, 3, 4, 5};
+        int[] newArray = new int[array.Length + 1];
+        int indexToInsert = 2;
+
+        Array.Copy(array, newArray, indexToInsert);
+        newArray[indexToInsert] = 10;
+
+        Array.Copy(array, indexToInsert, newArray, indexToInsert + 1, array.Length - indexToInsert);
+        Console.WriteLine(string.Join(", ", newArray));
+    }
+
+    public void Deletion(){
+
+    }
+
+    public void DynamicArrays(){
+
+    }
 }
