00ff91ae34312c36c19017de8de41691869aa00f
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2024-07-25 21:00:13 -0400

    accessing elements

---
+++ Service/random-practice/arrays/RandoPractice_Arrays.cs
@@ -0,0 +1,7 @@
+public class RandomPractice_Arrays{
+    public void AccessingElements(){
+        int[] array = {1, 2, 3, 4, 5};
+        int element = array[2];
+        Console.WriteLine(element);
+    }
+}
