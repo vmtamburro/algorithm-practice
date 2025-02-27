fab2b7f325a7c8a33a4834a36182f395a48d8db0
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2025-02-26 20:07:39 -0500

    Update 12. Longest Subarray Sum.cs
    Optimized O(N) time complexity, O(N) space complexity

--- Service/practice/12. Longest Subarray Sum.cs
+++ Service/practice/12. Longest Subarray Sum.cs
@@ -27,3 +27,28 @@ public int LongestSubarraySum(int[] arr, int k){
 
     return maxCount;
 } 
+
+//Input: nums = [1, -1, 5, -2, 3], k = 3  
+
+public int LogestArraySumOptimized(int[] arr, int k){
+    var sumIndexMap = new Dictionary<int, int>();
+    int maxLength = 0;
+    int cumulativeSum = 0;
+
+
+    for(int i = 0; i < arr.Length; i++){
+        cumulativeSum += arr[i];
+
+        if(cumulativeSum == k){
+            maxLength = i + 1;
+        }
+
+        if(!sumIndexMap.ContainsKey(cumulativeSum)){
+            sumIndexMap.Add(cumulativeSum, i);
+        }
+
+        if(sumIndexMap.ContainsKey(cumulativeSum - k)){
+            maxLength = Math.Max(maxLength, i - sumIndexMap[cumulativeSum - k]);
+        }
+    }
+}
